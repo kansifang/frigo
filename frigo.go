966dda425569dee83b7c91e1a5e93be1b4fe78f1
Zhigang Chen <zhigang.chen@moovweb.com>
2013-06-05 17:28:41 +0800

    change interface so it allows reuse tasks

--- frigo.go
+++ frigo.go
@@ -12,29 +12,9 @@ type Frigo struct {
 	friso C.friso_t
 }
 
-type task struct {
+type Task struct {
 	ft C.friso_task_t
-	input *C.char
-}
-
-func newTask(text string) *task {
-	t := &task{}
-	t.ft = C.friso_new_task()
-	t.input = C.CString(text)
-	C.friso_set_text(t.ft, t.input)
-	runtime.SetFinalizer(t, (*task).Free)
-	return t
-}
-
-func (t *task) Free() {
-	if t.ft != nil {
-		C.friso_free_task(t.ft)
-		t.ft = nil
-	}
-	if t.input != nil {
-		C.free(unsafe.Pointer(t.input))
-		t.input = nil
-	}
+	f C.friso_t
 }
 
 func NewFrigo(inifile string) *Frigo {
@@ -47,19 +27,42 @@ func NewFrigo(inifile string) *Frigo {
 }
 
 func (f *Frigo) Free() {
+	println("frigo freed")
 	if f.friso != nil {
 		C.friso_free(f.friso)
 		f.friso = nil
 	}
 }
 
-func (f *Frigo) Parse(text string) []string {
-	task := newTask(text)
-	words := make([]string, 0, 1)
-	for ; C.friso_next(f.friso, f.friso.mode, task.ft) != nil ; {
-		word := C.GoString(task.ft.hits.word)
-		words = append(words, word)
-		C.free(unsafe.Pointer(task.ft.hits.word))
+
+func (f *Frigo) NewTask() *Task {
+	t := &Task{f: f.friso}
+	t.ft = C.friso_new_task()
+	runtime.SetFinalizer(t, (*Task).Free)
+	return t
+}
+
+func (t *Task) Free() {
+	println("task freed")
+	if t.ft != nil {
+		C.friso_free_task(t.ft)
+		t.ft = nil
 	}
+	t.f = nil
+}
+
+func (t *Task) Parse(text string) []string {
+	input := C.CString(text)
+	defer C.free(unsafe.Pointer(input))
+	C.friso_set_text(t.ft, input)
+	words := make([]string, 0, 1)
+	for ; C.friso_next(t.f, t.f.mode, t.ft) != nil ; {
+		word := C.GoString(t.ft.hits.word)
+		words = append(words, word)
+		if t.ft.hits.wtype == C.__FRISO_NEW_WORDS__ {
+			C.free(unsafe.Pointer(t.ft.hits.word))
+		}
+	}
+
 	return words
 }
