370c2dbbe9be7150863256f7abac12429185e7ba
Zhigang Chen <zhigang.chen@moovweb.com>
2013-06-05 17:30:12 +0800

    memory test

--- frigo_test.go
+++ frigo_test.go
@@ -4,8 +4,17 @@ import "testing"
 
 func Test1(t *testing.T) {
 	f := NewFrigo("./friso.ini")
-	words := f.Parse("我爱北京天安门")
+	task := f.NewTask()
+	words := task.Parse("我爱北京天安门")
 	if len(words) != 4 {
 		t.Errorf("# of words does not match\n")
+	}
+}
+
+func TestMemory(t *testing.T) {
+	f := NewFrigo("./friso.ini")
+	for i := 0; i < 4000000; i ++ {
+		task := f.NewTask()
+		task.Parse("我爱北京天安门")
 	}
 }
