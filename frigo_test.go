6f17220d7ae044c8cb5f94dfbd6b0473afd9de49
Zhigang Chen <zhigang.chen@moovweb.com>
2013-06-08 13:17:34 +0800

    assume you copy the ini and dicc files to /etc/friso

--- frigo_test.go
+++ frigo_test.go
@@ -3,9 +3,21 @@ package frigo
 import "testing"
 
 func Test1(t *testing.T) {
-	f := NewFrigo("./friso.ini")
+	f := NewFrigo("/etc/friso/friso.ini")
 	task := f.NewTask()
 	words := task.Parse("我爱北京天安门")
+	if len(words) != 4 {
+		t.Errorf("# of words does not match %d != 4\n", len(words))
+	}
+}
+
+func Test2(t *testing.T) {
+	f := NewFrigo("/etc/friso/friso.ini")
+	task := f.NewTask()
+	words := task.Parse("面包 凯撒大帝 芝士球 照烧小丸子 85冰咖啡 小墨鱼 蓝莓奶酪 枫糖方块 丹麦奶酥菠萝 胚芽奶茶 起士球 抹茶蛋糕 泡芙")
+	for _, word := range(words) {
+		println(word)
+	}
 	if len(words) != 4 {
 		t.Errorf("# of words does not match\n")
 	}
